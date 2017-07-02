0f57b10081aa8c1d446d007d487419211df7860f
Jon Powell <jpowell68601@gmail.com>
2017-07-02 19:09:40 -0400

    Updated Movie Trailer Website

--- movie_trailer_proj/README.md
+++ movie_trailer_proj/README.md
@@ -4,5 +4,12 @@ The project is simply a website that displays all of my my favorite movies.
 
 ## Getting Started
 
-In order to checkout the website with all my favorite movies you will need to run the 'entertainment_center.py' file in your terminal. When you run this file it will dynamically render the website in the browser. You can the see all of my favorite movies and you can select any of the movies on the webpage to see the trailer of the movie yourself. 
+In order to checkout the website with all my favorite movies you will need to run the 'entertainment_center.py' file in your terminal. When you run this file it will dynamically render the website in the browser. You can the see all of my favorite movies and you can select any of the movies on the webpage to see the trailer of the movie yourself.
+
+Steps to run application:
+1. Download the files or fork the repo on Github
+2. Open a terminal or command line
+3. Run the command "python entertainment_center.py" from the "movie_trailer_proj" folder
+4. A browser should open with Movie Trailer Website
+5. Select a movie to see the trailer from Youtube 
 
--- movie_trailer_proj/entertainment_center.py
+++ movie_trailer_proj/entertainment_center.py
@@ -4,17 +4,17 @@ import media
 # Creates each instance from the Class Movie
 dark_knight_rises = media.Movie("The Dark Knight Rises",
                                 "Batman takes on his toughest adversary yet",
-                                "https://images-na.ssl-images-amazon.com/images/I/51k98elC6mL._SY450_.jpg",
+                                "https://images-na.ssl-images-amazon.com/images/I/51k98elC6mL._SY450_.jpg", #NOQA
                                 "https://www.youtube.com/watch?v=GokKUqLcvD8")
 
 inception = media.Movie("Inception",
                         "An expert of dream theory takes it to the next level",
-                        "http://cdn.collider.com/wp-content/uploads/Inception-movie-poster-7.jpg",
+                        "http://cdn.collider.com/wp-content/uploads/Inception-movie-poster-7.jpg", #NOQA
                         "https://www.youtube.com/watch?v=YoHD9XEInc0")
 
 force_awakens = media.Movie("The Force Awakens",
                             "A young girl learns about the force",
-                            "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2015/10/star-wars-force-awakens-official-poster.jpg",
+                            "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2015/10/star-wars-force-awakens-official-poster.jpg", #NOQA
                             "https://www.youtube.com/watch?v=sGbxmsDFVnE")
 
 ironman = media.Movie("Ironman",
@@ -24,14 +24,15 @@ ironman = media.Movie("Ironman",
 
 guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy",
                                       "A group of misfits save the galaxy",
-                                      "http://1.media.dorkly.cvcdn.com/26/95/18b149286ca6f2920e017bd5d2ffcbf5.jpg",
-                                      "https://www.youtube.com/watch?v=d96cjJhvlMA&t=24s")
+                                      "http://1.media.dorkly.cvcdn.com/26/95/18b149286ca6f2920e017bd5d2ffcbf5.jpg", #NOQA
+                                      "https://www.youtube.com/watch?v=d96cjJhvlMA&t=24s") #NOQA
 
 avengers = media.Movie("The Avengers",
-                       "The world's super heroes come together to stop its biggest threat",
-                       "https://s-media-cache-ak0.pinimg.com/736x/ee/51/b9/ee51b98cfe29f8f8943f09766398c606--avengers-movie-avengers-poster.jpg",
+                       "The world's super heroes come together to stop its biggest threat", #NOQA
+                       "https://s-media-cache-ak0.pinimg.com/736x/ee/51/b9/ee51b98cfe29f8f8943f09766398c606--avengers-movie-avengers-poster.jpg", #NOQA
                        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
 # Adds each instance to the template to be rendered
-movies = [dark_knight_rises, inception, force_awakens, ironman, guardians_of_the_galaxy, avengers]
+movies = [dark_knight_rises, inception, force_awakens,
+          ironman, guardians_of_the_galaxy, avengers]
 fresh_tomatoes.open_movies_page(movies)
